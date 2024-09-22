c92ceb67ec7cf15ed40163e871b8ba1d6096faed
Raghav chawla <raghchawla.131@gmail.com>
2024-09-22 11:52:01 +0530

    material test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,12 @@ function App() {
     <Canvas camera={{ position: [0, 0, 6], fov: 75 }} exposure={-0.99}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 10]} intensity={1} />
-      <Planet modelPath="/planets/sun.glb" meshName="Cube001" materialName="None" />
+      {/* <Planet modelPath="/planets/sun.glb" meshName="Cube001" materialName="None" /> */}
+      
       <Planet modelPath="/planets/earth.glb" meshName="Cube001" materialName="Default OBJ" />
-      <OrbitControls autoRotate={true} autoRotateSpeed={10} />
+      {/* <Planet modelPath="/planets/mercury.glb" meshName="Cube008" materialName="Default OBJ.005" /> */}
+
+      <OrbitControls autoRotate={true} autoRotateSpeed={3} />
     </Canvas>
   );
 } 
--- client/src/components/Planet.jsx
+++ client/src/components/Planet.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useGLTF } from '@react-three/drei';
-
+import * as THREE from 'three';
 const Planet = ({ modelPath, meshName, materialName, ...props }) => {
-    const { nodes, materials } = useGLTF(modelPath); // Adjust the path to your model
+    const { nodes, materials } = useGLTF(modelPath);
     console.log({modelPath}, nodes, materials);
     
+    // Temporarily use a basic material for visibility
+    const testMaterial = new THREE.MeshBasicMaterial({ color: 'green' });
+
     return (
-        <group {...props} dispose={null}>
+        <group {...props} dispose={null} scale={[3, 3, 3]}>
             <mesh
-                geometry={nodes[meshName].geometry} // Replace with your mesh name
-                material={materials[materialName]} // Replace with your material name
+                geometry={nodes[meshName].geometry}
+                material={testMaterial} // Use the test material
             />
         </group>
     );
 };
+
 
 export default Planet;
