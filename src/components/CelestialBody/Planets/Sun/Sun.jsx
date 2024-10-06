0456409b38768407dcbfb57c7e27ce1f5af9c12a
Raghav chawla <raghchawla.131@gmail.com>
2024-10-06 09:18:56 +0530

    pointlight sun

--- src/components/CelestialBody/Planets/Sun/Sun.jsx
+++ src/components/CelestialBody/Planets/Sun/Sun.jsx
@@ -13,19 +13,27 @@ const Sun = ({ textureUrl, radius, bloomLayer }) => {
   }, [bloomLayer]);
 
   return (
-    <>
+<>
+  <ambientLight intensity={0.3} /> {/* Add Ambient Light */}
+  {/* Add Point Light near the sun */}
+  <pointLight 
+    intensity={70} // Adjust the intensity of the light
+    position={[0, 0, 0]} // Position it at the sun's center
+    distance={1000} // Distance the light reaches
+    decay={1} // Adjust how the light fades with distance
+  />
   
-    <ambientLight intensity={1.5} />
-    <mesh ref={sunRef} layers={bloomLayer}>
-      <sphereGeometry args={[radius, 64, 64]} />
-      <meshStandardMaterial
-        map={texture}
-        emissiveMap={texture}
-        emissive={"white"}
-        emissiveIntensity={1.0}
-      />
-    </mesh>
-    </>
+  <mesh ref={sunRef} layers={bloomLayer}>
+    <sphereGeometry args={[radius, 64, 64]} />
+    <meshStandardMaterial
+      map={texture}
+      emissiveMap={texture}
+      emissive={"white"}
+      emissiveIntensity={1.0}
+    />
+  </mesh>
+</>
+
   );
 };
 
